2f53159c55d3c968e3bf927c469f27f36677d13a
richardjtelford <richard.telford@bio.uib.no>
2020-10-24 00:13:15 +0200

    geom_path, obviously

--- R/plot_grid.r
+++ R/plot_grid.r
@@ -6,7 +6,7 @@
 #' @details Sites are shown by dots, lines show which sites have the same value for environmental variables 1 and 2 in the simulated data.
 #' @note This function will be of little or no use outside of Bio303 indirect ordination practical as sites are rarely on a perfect grid.
 #' @importFrom vegan scores
-#' @importFrom ggplot2 ggplot aes geom_point geom_line coord_equal
+#' @importFrom ggplot2 ggplot aes geom_point geom_path coord_equal
 #' @importFrom rlang .data
 #' @examples 
 #' data(sim_env2)
@@ -38,9 +38,9 @@ plot_grid <- function(x, choices = 1:2){
   x$col_colour <- ifelse(x$col == 5, "blue", "grey40")
   
   ggplot(x, aes(x = .data[[dim_names[1]]], y = .data[[dim_names[2]]])) +
-    geom_line(aes(group = .data$col, colour = I(.data$col_colour)), 
+    geom_path(aes(group = .data$col, colour = I(.data$col_colour)), 
               show.legend = FALSE) +
-    geom_line(aes(group = .data$row, colour = I(.data$row_colour)), 
+    geom_path(aes(group = .data$row, colour = I(.data$row_colour)), 
               show.legend = FALSE) +
     geom_point() +
     coord_equal() 
