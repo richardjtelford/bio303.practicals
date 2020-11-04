0c013f3e75dbfed288719be54d79bb9f96745b84
richardjtelford <richard.telford@bio.uib.no>
2020-11-04 02:35:51 +0100

    ponds data

---
+++ R/ponds_data.R
@@ -0,0 +1,23 @@
+#' Ponds data
+#'
+#' @description Diatom relative abundances and environmental data in 30 English ponds
+#'
+#'@details   
+#' Datasets pond_spp and pond_env are data.frames containing diatoms species and environmental data on 30 English ponds.
+#' 
+#' @examples 
+#' data(ponds_env)
+#' data(ponds_spp)
+
+#' @references 
+#' Bennion, H. 1994. A diatom-phosphorus transfer function for shallow, eutrophic ponds in southeast England. _Hydrobiologia_ 275-76: 391-410
+#'    
+#' @name ponds_data
+#' @keywords datasets
+NULL
+
+
+#' @rdname ponds_data
+"ponds_env"
+#' @rdname ponds_data
+"ponds_spp"
